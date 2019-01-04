8e1c22545886a8f5fad0b110012f583e7b11d120
Nora <norapuskas@yahoo.com>
2019-01-04 14:43:33 +0100

    add functionality for arrow up & down

--- src/containers/GoldenAcornApp.js
+++ src/containers/GoldenAcornApp.js
@@ -10,26 +10,54 @@ class GoldenAcornApp extends Component {
     };
   }
   
+  componentDidMount() {
+    this.catchDocument();
+  }
+
   increase() {
     this.setState({
       counter: this.state.counter + 1,
-    })
+    });
   }
 
   decrease() {
     if (this.state.counter !== 0) {
       this.setState({
         counter: this.state.counter - 1,
-      })
+      });
     }
   }
+
+  handleKeyUp(event) {
+    console.log(event);
+    
+    if (event.keyCode === 38) {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    } else if (event.keyCode === 40 && this.state.counter !== 0) {
+      this.setState({
+        counter: this.state.counter - 1,
+      });
+    }
+  }
+
+  catchDocument() {
+    const myDocument = document.querySelector('body');
+    console.log(myDocument);
+    
+    myDocument.addEventListener('keyup', (event) => this.handleKeyUp(event))
+  }
+
 
   render() {
     return (
       <div>
-        <Button name="Buy one" onClick={() => this.increase()}/>
-        <Display><h2>{this.state.counter}</h2></Display>
-        <Button name="Eat one" onClick={() => this.decrease()}/>
+        <Button name="Buy one" onClick={(event) => this.increase(event)} />
+        <Display>
+          <h2>{this.state.counter}</h2>
+        </Display>
+        <Button name="Eat one" onClick={() => this.decrease()} />
       </div>
     );
   }
