b1236d206f4f6e8d0e77a26a656ff32b51dee32b
Nora <norapuskas@yahoo.com>
2019-01-06 19:50:06 +0100

    add headline component

--- src/components/AcornRouter.js
+++ src/components/AcornRouter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import GoldenAcornApp from '../containers/GoldenAcornApp';
 import Norlandia from './Norlandia';
+import Headline from '../components/Headline';
 
 const AcornRouter = () => {
   return (
     <Router>
       <div>
+        <Headline />
         <ul>
           <li>
             <Link to="/norlandia">Norlandia</Link>
---
+++ src/components/Headline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Headline = () => {
+  return (
+    <div>
+      <h1>🌰🌰🌰 Golden Acorn is life! 🌰🌰🌰</h1>
+    </div>
+  );
+}
+ 
+export default Headline;
